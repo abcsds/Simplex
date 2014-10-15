1eeef9b0e4579f81e98b5ab42a5d619d967704c7
Alberto Barradas <abcsds@gmail.com>
2014-10-15 11:08:32 -0500

    Need to do Basic Solutions

--- Python/simplex.py
+++ Python/simplex.py
@@ -18,11 +18,50 @@ def simplex(C,A,b):
     # print costs
 
     tabular = vstack((parcial,costs))
-    print tabular
-    # tabular = array([array([A,eye(m),b.T]),array([-C,zeros((1,m+1))])])
-    # print "TABULAR : " , tabular
+    # print tabular
+    
+    #TODO
+    bv = array([n+1,m+n])# Index of Basic Variables
+    nbv = array([1,n]) # Index of Non-Basic Variables
+    print tabular[0:m, cols]
+    # bs(bv) = tabular(1:m,cols);  # basic solutions = Value of basic variables
 
 
+    ##  LOOP
+    # while 1
+    #     in = optimalityCondition(tabular(rows,nbv));
+    #     if in == -1
+    #         break;
+    #     end
+    #     c_pivot = nbv(in);
+
+    #     out = factibilityCondition(tabular(1:m,cols)./tabular(1:m,c_pivot));
+    #     if out == -1
+    #         error('No solution');
+    #     end
+    #     r_pivot = out;
+    #     den = tabular(r_pivot, c_pivot);
+    #     tabular(r_pivot,:) = tabular(r_pivot,:)/den;
+    #     % Change basic variables for next iteration
+    #     aux = nbv(in);
+    #     nbv(c_pivot) = bv(out);
+    #     bv(out) = aux;
+    #     % Clear for i:/:rows
+    #     for i=1:rows
+    #         if i == r_pivot
+    #             continue;
+    #         end
+    #         fact = tabular(i,c_pivot);
+    #         % To see steps remove the semi-colon here
+    #         tabular(i,:) = tabular(i,:)-fact*tabular(r_pivot,:);
+    #     end
+    #     bs(bv) = tabular(1:m,cols);
+    #     bs(nbv) = 0;
+
+    # end
+
+    x= bs
+    # z = tabular(rows,cols)
 
 
-    return [m,n]; # TODO remove
+    return x,z
